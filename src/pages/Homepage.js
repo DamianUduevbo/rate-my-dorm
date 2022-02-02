4d67fb06891a79d440d789600330d28c8512035b
Damian526 <58115973+Damian526@users.noreply.github.com>
2022-02-02 11:23:56 -0500

    A1

---
+++ src/pages/Homepage.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import logo from '../components/images/logo.svg';
+import SearchBar from '../components/SearchBar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Schools from './Schools';
+
+const Homepage = () => {
+  return (
+    <div>
+
+        <h1>
+            <p>Welcome to RateMyDorms</p>
+            <SearchBar placeholder="Search for a college"></SearchBar> {/*REPLACE W A DROPDOWN*/}
+            <input type="submit" />
+        </h1>
+        <img src={logo} className="App-logo" alt="logo" />
+        <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer">
+            Learn React
+        </a>
+        <button onClick={() => console.log("Hello")}>Browse Schools</button>
+
+    </div>)
+};
+
+export default Homepage;
