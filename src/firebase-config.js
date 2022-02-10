0fc7157aa41830e6057ed2e79e14c45b9dca3289
Damian526 <58115973+Damian526@users.noreply.github.com>
2022-02-09 19:06:50 -0500

    A8

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,7 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore"
 
 const firebaseConfig = {
-    
+    apiKey: "AIzaSyAdS7AaqaGYqSGMh3ShnfwBIclpMaMp_ug",
+    authDomain: "ratemy-3cd1e.firebaseapp.com",
+    projectId: "ratemy-3cd1e",
+    storageBucket: "ratemy-3cd1e.appspot.com",
+    messagingSenderId: "716020254386",
+    appId: "1:716020254386:web:88fd2f0b4f7007c4902bab"
   };
 
 const app = initializeApp(firebaseConfig);
--- src/pages/Schools.js
+++ src/pages/Schools.js
@@ -2,6 +2,7 @@ import React from 'react';
 import neu from '../components/images/neu.png';
 import bu from '../components/images/bu.png';
 import Button from '../components/Button';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
 const Schools = () => {
     return <div>
@@ -9,6 +10,7 @@ const Schools = () => {
         <Button id="neu-boston" style={ {color: 'white', backgroundColor: 'transparent'} }
                 src={neu} alt="Northeastern-University.png"
                 schoolName="Northeastern University" />
+        <Link to="/school-page" type='button' src={neu}> Northeastern University </Link>
         {" \n"}
         <Button id="bu-boston" style={ {color: 'white', backgroundColor: 'transparent'} }
                 src={bu} alt="Boston-University.png" 
